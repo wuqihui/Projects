cc5647ba7b7b5b9ea61cc1633cccde16fcb071c4
wuqihui <wu_qihui@163.com>
2015-07-31 02:43:09 +0800

    添加后台管理项目

---
+++ GPMS/Common/GPMS.Core.Entities/AdvertisementInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GPMS.Core.Entities.Enums;
+
+namespace GPMS.Core.Entities
+{
+    /// <summary>
+    /// 广告信息
+    /// </summary>
+    public class AdvertisementInfo : BaseEntity<Guid>
+    {
+        /// <summary>
+        /// 类型
+        /// </summary>
+        public virtual AdvertisementTypes Type { get; set; }
+    }
+}
---
+++ GPMS/Common/GPMS.Core.Entities/Enums/AdvertisementTypes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPMS.Core.Entities.Enums
+{
+    /// <summary>
+    /// 广告图种类
+    /// </summary>
+    public enum AdvertisementTypes
+    {
+        /// <summary>
+        /// 注册页面广告
+        /// </summary>
+        RegisterPage=0,
+        /// <summary>
+        /// 登录页面广告
+        /// </summary>
+        LoginPage=1
+    }
+}
